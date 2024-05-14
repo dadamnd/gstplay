2e9261396df0587d416d02ed521006c5fd3a4227
dadamnd <xunny@daum.net>
2024-05-14 16:00:35 +0900

    simple code

--- gstplay.c
+++ gstplay.c
@@ -1,29 +1,8 @@
-#include <gst/gst.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    GstElement *pipeline;
-    GstBus *bus;
-    GstMessage *msg;
+int main(void)
+{
+	printf("Hello great yocto!\n");
 
-    /* Initialize GStreamer */
-    gst_init(&argc, &argv);
-
-    /* Create the elements */
-    pipeline = gst_parse_launch("v4l2src device=/dev/video2 ! waylandsink", NULL);
-
-    /* Start playing */
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-
-    /* Wait until error or EOS */
-    bus = gst_element_get_bus(pipeline);
-    msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
-
-    /* Free resources */
-    if (msg != NULL)
-        gst_message_unref(msg);
-    gst_object_unref(bus);
-    gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(pipeline);
-
-    return 0;
-}
+	return 0;
+}	
